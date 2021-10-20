b45f3b26bb21317d4ed0dd1395f195f55fa3610c
Brett Huffman <dicer2000@yahoo.com>
2021-10-20 13:22:23 -0500

    Adjusted action

--- tests/unit/arrayShuffle.test.js
+++ tests/unit/arrayShuffle.test.js
@@ -4,7 +4,7 @@
 const { testUrl } = require('../../agxgame.js');
 
 //describe divide our tests in sections
-describe('shuffle function -> testUrl', () => {
+describe('agxgame function -> shuffle', () => {
     test('passing nothing to function shuffle - EXPECT ERROR MESSAGE',                      async () => {
     await expect(shuffle(null)).toBeNull();
     });
